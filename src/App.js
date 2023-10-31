d0a0bfd14a263ec60dfb30082746dc8934159f09
Krzysztof Wyszyński <wukrzychu@gmail.com>
2023-10-31 22:40:46 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,95 @@
+import { useEffect, useState } from "react";
+import "./styles.css";
+
+export class Store {
+  subs = [];
+
+  constructor(initialState) {
+    this.state = initialState;
+  }
+
+  subscribe(sub) {
+    this.subs.push(sub);
+  }
+
+  unsubscribe(sub) {
+    this.subs.filter((s) => s !== sub);
+  }
+
+  dispatch(setState) {
+    const newState = setState(this.state);
+    this.state = newState;
+    this.subs.forEach((sub) => sub(newState));
+  }
+}
+
+export const useSelector = (selectorFn) => {
+  const [value, setValue] = useState(selectorFn(store.state));
+
+  const getValue = (state) => {
+    setValue(selectorFn(state));
+  };
+
+  useEffect(() => {
+    store.subscribe(getValue);
+    return () => {
+      store.unsubscribe(getValue);
+    };
+  }, []);
+
+  return value;
+};
+
+const store = new Store({
+  firstName: "marek",
+  lastName: "tadek",
+});
+
+const Comp1 = () => {
+  const firstName = useSelector((state) => state.firstName);
+
+  const handleClick = () => {
+    store.dispatch((currentState) => ({
+      ...currentState,
+      firstName: "zenek",
+    }));
+  };
+
+  return (
+    <div style={{ marginBottom: "1rem" }}>
+      Value: {firstName}
+      <button style={{ marginLeft: "5px" }} onClick={handleClick}>
+        Click me
+      </button>
+    </div>
+  );
+};
+
+const Comp2 = () => {
+  const lastName = useSelector((state) => state.lastName);
+
+  const handleClick = () => {
+    store.dispatch((currentState) => ({
+      ...currentState,
+      lastName: "mareczek",
+    }));
+  };
+
+  return (
+    <>
+      Value: {lastName}
+      <button style={{ marginLeft: "5px" }} onClick={handleClick}>
+        Click me
+      </button>
+    </>
+  );
+};
+
+export default function App() {
+  return (
+    <div className="App">
+      <Comp1 />
+      <Comp2 />
+    </div>
+  );
+}
